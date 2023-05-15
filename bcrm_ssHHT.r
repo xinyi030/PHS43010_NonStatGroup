7c1c352dd045e255ca43ebe6cc811be90fb9c5fc
sirius2713 <aaronzh99@gmail.com>
2023-05-15 12:37:00 -0500

    bcrm sim results under scenario1

--- bcrm_ssHHT.r
+++ bcrm_ssHHT.r
@@ -7,7 +7,8 @@ library(rjags)
 library(R2WinBUGS)
 
 dose <- c(0.5, 1, 3, 5, 6)
-truep <- c(0.01, 0.05, 0.2, 0.3, 0.5)
+truep1 <- c(0.25, 0.3, 0.5, 0.6, 0.7)
+truep2 <- c(0.01, 0.05, 0.2, 0.3, 0.5)
 p.tox0 <- c(0.05, 0.10, 0.15, 0.33,0.50) 
 nsims <- 1000
 
@@ -17,7 +18,7 @@ mu <- c(2.15, 0.52)
 Sigma <- rbind(c(0.84^2, 0.134), c(0.134, 0.80^2))
 bcrm.sim <- bcrm(stop=list(nmax=36), dose=dose, ff="logit2", p.tox0 = p.tox0
 , prior.alpha=list(4, mu, Sigma), target.tox=target.tox, constrain=TRUE, pointest="mean", 
-start=1, simulate=TRUE, nsims=nsims, truep=truep, method="rjags")
+start=1, simulate=TRUE, nsims=nsims, truep=truep1, method="rjags")
 
 # define a two-parameter logistic function
 logit2 <- function(x, alpha, beta) {
@@ -41,4 +42,5 @@ for (i in 1:nsims) {
 
 table(mtds)/1000
 print(bcrm.sim)
-plot(bcrm.sim, trajectories=FALSE,  file="bcrm_ssHHT_scenario2_summary.pdf")
+plot(bcrm.sim, trajectories=FALSE,  file="bcrm_ssHHT_scenario1_summary.pdf")
+plot(bcrm.sim, trajectories=TRUE,  file="bcrm_ssHHT_scenario1_trajectories.pdf")
